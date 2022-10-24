85b560163544e063fa5a253969b81a054f67071d
unknown <oguzhandemiraslan@gmail.com>
2022-10-24 15:15:10 -0400

    messages stored in database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,10 +1,12 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
     if (
       !email ||
-      email.includes("@") ||
+      !email.includes("@") ||
       !name ||
       name.trim() === "" ||
       !message ||
@@ -14,18 +16,38 @@ function handler(req, res) {
       return;
     }
 
-    //Store it
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(process.env.MONGODB);
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
-      .json({ message: "Message sent successfully", message: newMessage });
+      .json({ message: "Successfully stored message!", message: newMessage });
   }
 }
+
 export default handler;
